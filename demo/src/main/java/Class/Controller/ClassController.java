a7b2863977fa481f82a42339a5b76edfb5df53b8
post187 <doandang22082005@gmail.com>
2026-02-06 23:50:18 +0700

    Class Controller

--- demo/src/main/java/Class/Controller/ClassController.java
+++ demo/src/main/java/Class/Controller/ClassController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/classes")
+@RequestMapping("/classes")
 @RequiredArgsConstructor
 @Slf4j
 public class ClassController {
